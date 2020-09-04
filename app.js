a14d5a7ec9fdd4fb75d129714ed551037caecd4a
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2020-09-04 22:12:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
-// Take 're' as a constant(const)
-// And validate that string with regular expression
-// In all these functions
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Bonus Points if you can convert these funtion in and arrow funtions
-
 function validateName() {
   const name = document.getElementById('name');
-  // re
-  //name must be alphabet and in between 2 to 20 charcters
+   //name must be alphabet and in between 2 to 20 charcters
   const re = /^([a-zA-Z]{2,20}$)/;
   
   if(!re.test(name.value)){
@@ -25,7 +18,6 @@ function validateName() {
 
 function validateZip() {
   const zip = document.getElementById('zip');
-  // re
   // zipcode must be 6 digit number 
   const re = /^([0-9]{6}$)/;
   if(!re.test(zip.value)){
@@ -37,7 +29,6 @@ function validateZip() {
 
 function validateEmail() {
   const email = document.getElementById('email');
-  //  re
   // email must in the form of abc@gmail/yahoo.com/in/media
   const re = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
   if(!re.test(email.value)){
@@ -49,7 +40,6 @@ function validateEmail() {
 
 function validatePhone() {
   const phone = document.getElementById('phone');
-  // re
   // phoneNum must be 10  digit number
   const re = /^([0-9]{10}$)/;
 
